ab731f49ee9861db750dff4bf31fbde931555436
connorq <connorquitt@gmail.com>
2023-09-26 02:51:14 -0500

    made timer work

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+    //global variables
+    const clicker = document.querySelector("#red-clicker")
+    const points = document.querySelector("#points")
+    let clicks = 0
+    let timer = document.querySelector("#timer")
+
     //makes username and team select store values
-    document.querySelector("#submit-btn").addEventListener("click", ()=>{
+    document.querySelector("#start-btn").addEventListener("click", ()=>{
         let username = document.querySelector("#username").value
         let team = document.querySelector("#click-drop").value
         document.querySelector("#username").value = ''
         document.querySelector("#click-drop").value = ''
+        //makes timer start
+        const gameTimer = setInterval(countdown, 1000)
+        gameTimer
+        
+        
     })
+
+    //makes button work and stores points
+   
 })
+
+    //functions
+        
+        //timer
+    function countdown(){
+        if(timer.textContent == 0) {
+            clearTimeout(gameTimer)
+        }else {
+            timer.textContent--
+        }
+    }
