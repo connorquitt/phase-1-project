5c53d5f7c33a76f7c6ac5656427bd86f01983078
connorq <connorquitt@gmail.com>
2023-09-28 13:19:14 -0500

    server is updating scores

--- index.js
+++ index.js
@@ -36,10 +36,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
     setInterval(function checkTimer() {
         if(timer.textContent == 0){
             clearTimeout()
-            let finalScore = clicks
-            finalMsg.textContent = `Congrats ${username.textContent} on scoring ${finalScore}! Please select which team to donate them to`
+            finalMsg.textContent = `Congrats ${username.textContent} on scoring ${clicks}! Please select which team to donate them to`
             retryBtn.classList.remove("hidden")
-            scoreboard.classList.remove("hidden")
+            document.querySelector("#score-hold").classList.remove("hidden")
         }
     }, 100)
 
@@ -50,12 +49,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
         //scoreboard update from points scored
         function updateScore(team) {
-            fetch(`ttp://localhost:3000/teams${team.teamName}`, {
+            fetch(`http://localhost:3000/teams/${team.teamName}`, {
             method: 'PATCH',
             headers:{
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify()
+            body: JSON.stringify(team)
         })
         .then(res => res.json(team))
         .then(data => console.log(data))
@@ -74,20 +73,24 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
         //creates one team scorecard
         function makeScoreCard(e){
+            let teamPoints = e.score
+                teamPoints.id = 'current-points'
             let teamScore = document.createElement('li')
-                    teamScore.innerHTML = `${e.teamName} has: ${e.score} points!`
+                    teamScore.textContent = `${e.teamName} has: ${e.score} points!`
                 let updatePoints = document.createElement('button')
-                    updatePoints.className = 'button'
+                    updatePoints.className = 'update-button'
                     updatePoints.id = 'submit-score'
                     updatePoints.textContent = 'Select Team'
                 teamScore.appendChild(updatePoints)
                 scoreboard.appendChild(teamScore)
-                teamScore.querySelector("#submit-score").addEventListener('click', ()=>console.log("click"))
+                teamScore.querySelector("#submit-score").addEventListener('click', ()=> {
+                    e.score += clicks
+                    clicks = 0
+                    teamScore.textContent = `${e.teamName} has: ${e.score} points!`
+                    updateScore(e)
+                })
         }
 
         
    
 })
-
-//want to use foreach instead to turn them into a type of card to show as a leaderboard and then i can still send a patch request to update the score on the cards
-//like i've already done :)
